edb44047a25666c01c22555fe89491ad1860dc39
ssokov <sokovykh.work@gmail.com>
2025-12-17 13:53:47 +0300

    feat: edit format api

--- middleware/convertor.go
+++ middleware/convertor.go
@@ -1,77 +1,66 @@
 package middleware
 
-import "github.com/go-telegram/bot/models"
+import (
+	"strconv"
 
-func ToModelsUpdate(update models.Update) ModelsUpdate {
-	result := ModelsUpdate{
-		Update_id: int(update.ID),
-	}
+	"github.com/go-telegram/bot/models"
+)
 
-	if update.Message != nil {
-		msg := toModelsMessage(update.Message)
-		result.Message = &msg
-	}
-
-	if update.EditedMessage != nil {
-		msg := toModelsMessage(update.EditedMessage)
-		result.Edited_message = &msg
-	}
-
-	if update.ChannelPost != nil {
-		msg := toModelsMessage(update.ChannelPost)
-		result.Channel_post = &msg
-	}
-
-	if update.EditedChannelPost != nil {
-		msg := toModelsMessage(update.EditedChannelPost)
-		result.Edited_channel_post = &msg
+func ToModelsUpdate(update models.Update, serviceName string) Log {
+	strTimestamp := strconv.Itoa(update.Message.Date)
+	result := Log{
+		Type:       "Telegram Update",
+		Error_code: 200,
+		Tg_user_id: int(update.Message.From.ID),
+		Timestamp:  strTimestamp,
+		Service_name: serviceName,
 	}
 
 	return result
 }
 
-func toModelsMessage(msg *models.Message) ModelsMessage {
-	result := ModelsMessage{
-		Message_id: msg.ID,
-		Date:       msg.Date,
-		Text:       msg.Text,
-		Caption:    msg.Caption,
-	}
 
-	result.Chat = toModelsChat(&msg.Chat)
 
-	if msg.From != nil {
-		user := toModelsUser(msg.From)
-		result.From = &user
-	}
 
-	return result
-}
 
-func toModelsChat(chat *models.Chat) ModelsChat {
-	return ModelsChat{
-		ID:         int(chat.ID),
-		Type:       string(chat.Type),
-		Title:      chat.Title,
-		Username:   chat.Username,
-		First_name: chat.FirstName,
-		Last_name:  chat.LastName,
-		Is_forum:   chat.IsForum,
-	}
-}
 
-func toModelsUser(user *models.User) ModelsUser {
-	return ModelsUser{
-		ID:                          int(user.ID),
-		Is_bot:                      user.IsBot,
-		First_name:                  user.FirstName,
-		Last_name:                   user.LastName,
-		Username:                    user.Username,
-		Language_code:               user.LanguageCode,
-		Is_premium:                  user.IsPremium,
-		Added_to_attachment_menu:    user.AddedToAttachmentMenu,
-		Can_join_groups:             user.CanJoinGroups,
-		Can_read_all_group_messages: user.CanReadAllGroupMessages,
-		Support_inline_queries:      user.SupportInlineQueries,
-	}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,20 +1,19 @@
 package middleware
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
 type LogMiddleware struct {
-	client   Client
-	endpoint string
+	client      Client
+	endpoint    string
+	serviceName string
+	// [TODO] api key
 }
 
 type BadStatusCodeError struct {
@@ -36,10 +35,11 @@ func (e *BadStatusCodeError) Is(target error) bool {
 }
 
 // endpoint - URL бэкенда для отправки логов
-func NewLogMiddleware(endpoint string) *LogMiddleware {
+func NewLogMiddleware(endpoint string, ServiceName string) *LogMiddleware {
 	return &LogMiddleware{
-		endpoint: endpoint,
-		client:   *NewClient(endpoint, nil),
+		endpoint:    endpoint,
+		serviceName: ServiceName,
+		client:      *NewClient(endpoint, nil),
 	}
 }
 
@@ -53,7 +53,7 @@ func (l *LogMiddleware) TelegramLogMiddleware(next bot.HandlerFunc) bot.HandlerF
 						log.Printf("panic log middleware: %v", r)
 					}
 				}()
-				convertedUpdate := ToModelsUpdate(*update)
+				convertedUpdate := ToModelsUpdate(*update, l.serviceName)
 				err := l.client.Logs.AddTelegramLog(ctx, convertedUpdate)
 				if err != nil {
 					log.Printf("Failed send log to backend. err: %v", err)
@@ -63,29 +63,4 @@ func (l *LogMiddleware) TelegramLogMiddleware(next bot.HandlerFunc) bot.HandlerF
 		}
 		next(ctx, bot, update)
 	}
-}
-
-func sendTelegramLog(ctx context.Context, endpoint string, log *models.Update) error {
-	data, err := json.Marshal(log)
-	if err != nil {
-		return fmt.Errorf("failed marshal telegram log. err: %w", err)
-	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(data))
-	if err != nil {
-		return fmt.Errorf("failed create request. err: %w", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed send request. err: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return &BadStatusCodeError{Code: resp.StatusCode}
-	}
-
-	return nil
 }
