5ae361c29ab9c3af6ce543947ea8c9badd09d9e3
ssokov <sokovykh.work@gmail.com>
2025-12-17 14:54:12 +0300

    fix: add required params

--- middleware/convertor.go
+++ middleware/convertor.go
@@ -10,6 +10,8 @@ func ToModelsUpdate(update models.Update, serviceName string) Log {
 	strTimestamp := strconv.Itoa(update.Message.Date)
 	result := Log{
 		Type:       "Telegram Update",
+		Message: update.Message.Text,
+		Tg_nickname: update.Message.From.Username,
 		Error_code: 200,
 		Tg_user_id: int(update.Message.From.ID),
 		Timestamp:  strTimestamp,
